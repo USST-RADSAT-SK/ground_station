9adfbf62893ec24b3e843f96982911a6d17c2c67
Austin Hruska <Austin.Hruska@usask.ca>
2022-06-16 05:18:13 -0600

    Added a bunch of folders and initial files.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='RADSATGS',
+      version='0.0.0',
+      description='Ground station software for the RADSAT-SK CubeSat',
+      url='https://github.com/USST-RADSAT-SK/ground_station',
+      author='USST',
+      author_email='cubesat@usst.ca',
+      #license='MIT',
+      packages=['radsatGs'],
+      zip_safe=False)
