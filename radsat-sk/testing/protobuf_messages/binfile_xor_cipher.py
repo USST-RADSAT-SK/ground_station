5e34e34a0d8d08d6a5b506d9807af3fede7029de
Gedeon Isezerano <gedeon_isezerano@Gedeons-MacBook-Pro.local>
2022-07-21 16:11:41 -0600

    xor_cipher file modified to read in key from file

--- radsat-sk/testing/protobuf_messages/binfile_xor_cipher.py
+++ radsat-sk/testing/protobuf_messages/binfile_xor_cipher.py
@@ -2,24 +2,24 @@ from os import urandom
 
 """
 -This file contains three function:
-    - genkey() : It is used to create an arbitrary key for testing purposes
+    - genkey() : It is used to create an arbitrary key for testing purposes -- Uncomment it when you want to generate a key
     - xor_cipher(): It is used to cipher a binary file 
     - xor_decipher(): It is used to decipher a binary file
 """
 
 
 #random key generating function for testing purposes
-def genkey(length):
-    """Generate key in bytes"""
-    return urandom(length)
+# def genkey(length):
+#     """Generate key in bytes"""
+#     return urandom(length)
 
-#Generate key and store it in a file
-key = genkey(10)
-try:
-        with open("encryption_key",'wb')as file:
-            file.write(key)
-except:
-    print("binary file key was not successfully created")
+# #Generate key and store it in a file
+# key = genkey(10)
+# try:
+#         with open("encryption_key",'wb')as file:
+#             file.write(key)
+# except:
+#     print("binary file key was not successfully created")
 
 
 
@@ -40,9 +40,16 @@ def xor_cipher(file, key):
     except:
         print("An error occured with opening the file")
 
+     #open key to use for cipher
+    try:
+        with open(key,"rb") as f:
+            k = f.read()
+    except:
+        print("An error occured with opening the file")
+
     #perform cipher
     for i in binary_list:
-        encrypted_binary.append(i ^key[0])
+        encrypted_binary.append(i ^k[0])
     #create file to store cipher in the same directory
     try:
         with open("encrypted_msg",'wb')as file:
@@ -59,14 +66,22 @@ def xor_decipher(file,key):
     with open(file,"rb") as f:
         binary_list = list(f.read())
 
+     #open key to use for cipher
+    try:
+        with open(key,"rb") as f:
+            k = f.read()
+    except:
+        print("An error occured with opening the file")
+
     for i in binary_list:
-        decrypted_binary.append(i ^key[0])
+        decrypted_binary.append(i ^k[0])
     return decrypted_binary
 
             #Quick test: Insert test bin files
-# check1 = xor_cipher("output-onlinefiletools.bin",key)
-# check2 = xor_decipher("encrypted_msg",key)
-
+# check1 = xor_cipher("output-onlinefiletools.bin","encryption_key")
+# check2 = xor_decipher("encrypted_msg","encryption_key")
+# print(check1)
+# print(check2)
 # for i in range(0,len(check1)):
 #     if check1[i]!=check2[i]:
 #         print("Encryption failed")
