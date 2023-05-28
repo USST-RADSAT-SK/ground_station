a0a43a0399ac2fcab08c49967a61784b24c80d24
Austin <Austin.Hruska@usask.ca>
2023-05-28 13:30:50 -0600

    Yaesu read working better. Needs more testing...

--- testing/gPredictPassApprover.py
+++ testing/gPredictPassApprover.py
@@ -1,15 +1,10 @@
 import serial
 import socket
-from time import sleep
 
 def connectToRotator():
     try:
         s = serial.Serial(port = "/dev/ttyUSB0", baudrate = 9600, timeout = 1)
-        s.write(("r\r\n").encode())
-        sleep(1)
-        s.write(("l\r\n").encode())
-        sleep(1)
-        s.write(("s\r\n").encode())
+        s.write(("\r\n\r\n\r\n\r\n\r\n\r\n").encode())
         print("Connection succesful")
     except Exception as e:
         print("Rotator Error :",e)
@@ -36,17 +31,19 @@ if __name__ == "__main__":
     try:
 
         while True:
+            # Initial values to init with before getting real ones
             az = "180.00"
             el = "45.00"
             yaz = "180"
             yel = "045"
 
+            # Number of posotion requests from Gpredict to avoid overwhelming yaesu buffer
+            nReq = 0
+
             client,address = server.accept()
-            print(client)
-            print(address)
             
             while True:
-                recvCommand = client.recv(512).decode().strip("\n")
+                recvCommand = client.recv(1024).decode().strip("\n")
                 print("Got : <%s>" % recvCommand)
 
                 if "S" in recvCommand:
@@ -61,22 +58,33 @@ if __name__ == "__main__":
                     print("Requested Az = <%s>, El = <%s>" % (az,el))
                     client.send(("RPRT 0\n").encode())
 
-                    yaesu.write(("W" + f'{int(float(az)):03}' + " " + f'{int(float(el)):03}' + "\n").encode("ascii"))
-                
+                    yaesu.write(("W" + f'{int(float(az)):03}' + " " + f'{int(float(el)):03}' + "\r\n").encode())
+                    yaesu.readline(1024)
+
                 elif "p" in recvCommand:
-                    yaesu.write(("c2\r\n").encode("ascii"))
-                    azel = yaesu.readline(512).decode("ascii").strip("\n")
-                    print("Yaesu :",azel)
-                    if azel[3:6] != "" or azel[11:] != "":
-                        yaz = azel[3:6]
-                        yel = azel[11:]
-                    print("Sending: Az = <%s>, El = <%s>" % (yaz,yel))
+                    if nReq == 0:
+                        nReq = 10
+                        yaesu.write(("c2\r\n").encode())
+                        azel = yaesu.readline(1024).decode().strip("\n")                    
+                        if "?>" in azel:
+                            yaesu.write(("c2\r\n").encode())
+                            azel = yaesu.readline(1024).decode().strip("\n")
+                        
+                            print("Yaesu :",azel)
+
+                        if azel[3:6] != "" or azel[11:] != "":
+                            yaz = azel[3:6]
+                            yel = azel[11:]
+
+                        print("Sending: Az = <%s>, El = <%s>" % (yaz,yel))
+
+                    nReq = nReq-1
+
                     client.send((yaz + "\n" + yel + "\n").encode())
                 
-                yaesu.write(("\r\n").encode("ascii"))
 
     except Exception as e:
         print("Encountered exception :",e)
         yaesu.close()
         server.close()
-        assert()
+        exit()
